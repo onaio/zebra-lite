26c7640eab4f7f6b721e2df7c89057812ce98ec6
pld <peter@helioid.com>
2015-05-06 17:53:43 -0400

    PLD: add osmtogeojson dep

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defn js-dir
-      "Prefix with full JavaScript directory."
-      [path]
-      (str "resources/public/js/" path))
+  "Prefix with full JavaScript directory."
+  [path]
+  (str "resources/public/js/" path))
 
 (defproject onaio/zebra-lite "0.1.0-SNAPSHOT"
   :description "A lite Zebra Version"
   :url "https://github.com/onaio/zebra-lite"
   :license {:name "Apache 2 License"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [;; ZEBRA LITE REQUIREMENTS
-                 [onaio/milia "0.1.0-SNAPSHOT"]
+  :dependencies [[onaio/milia "0.1.0-SNAPSHOT"]
+                 [prabhasp/osmtogeojson-cljs "2.2.5-SNAPSHOT"]
                  [org.clojars.onaio/hatti "0.1.0-SNAPSHOT"]
                  [org.omcljs/om "0.8.8"]
                  [com.google.guava/guava "16.0"]
@@ -36,7 +36,6 @@
                  [clj-redis-session "2.1.0"]
                  ;; CLSJS Dependencies
                  [cljsjs/moment "2.9.0-0"]]
-
   :plugins [[com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure]]
             [lein-cljsbuild "1.0.5"]
             [lein-midje "3.1.3"]
@@ -69,35 +68,38 @@
                  "target/generated/src/cljs"]
   :test-paths ["tests/clj" "target/generated/tests/clj"]
   :cljsbuild {
-    :builds {:dev
-              {:source-paths ["src/cljs"
-                              "target/generated/src/cljs"]
-               :compiler {:output-to ~(js-dir "lib/main.js")
-                          :output-dir ~(js-dir "lib/out")
-                          :optimizations :whitespace
-                          :pretty-print true
-                          :source-map ~(js-dir "lib/main.js.map")}}
-              :test
-              {:source-paths ["src/cljs"
-                              "tests/cljs"
-                              "target/generated/src/cljs"]
-               :notify-command ["phantomjs"
-                                "phantom/unit-test.js"
-                                "phantom/unit-test.html"
-                                "target/main-test.js"]
-               :compiler {:output-to "target/main-test.js"
-                          :optimizations :whitespace
-                          :pretty-print true}}
-              :prod
-              {:source-paths ["src/cljs"
-                              "target/generated/src/cljs"]
-               :compiler {:output-to ~(js-dir "lib/main.js")
-                          :output-dir ~(js-dir "lib/out-prod")
-                          :optimizations :advanced
-                          :pretty-print false}
-               :jar true}}
-    :test-commands {"unit-test"
-                    ["phantomjs" "phantom/unit-test.js" "phantom/unit-test.html" "target/main-test.js"]}}
+              :builds {:dev
+                       {:source-paths ["src/cljs"
+                                       "target/generated/src/cljs"]
+                        :compiler {:output-to ~(js-dir "lib/main.js")
+                                   :output-dir ~(js-dir "lib/out")
+                                   :optimizations :whitespace
+                                   :pretty-print true
+                                   :source-map ~(js-dir "lib/main.js.map")}}
+                       :test
+                       {:source-paths ["src/cljs"
+                                       "tests/cljs"
+                                       "target/generated/src/cljs"]
+                        :notify-command ["phantomjs"
+                                         "phantom/unit-test.js"
+                                         "phantom/unit-test.html"
+                                         "target/main-test.js"]
+                        :compiler {:output-to "target/main-test.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}
+                       :prod
+                       {:source-paths ["src/cljs"
+                                       "target/generated/src/cljs"]
+                        :compiler {:output-to ~(js-dir "lib/main.js")
+                                   :output-dir ~(js-dir "lib/out-prod")
+                                   :optimizations :advanced
+                                   :pretty-print false}
+                        :jar true}}
+              :test-commands {"unit-test"
+                              ["phantomjs"
+                               "phantom/unit-test.js"
+                               "phantom/unit-test.html"
+                               "target/main-test.js"]}}
   :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/generated/src/clj"
