e145b00296eb39bfbc5a51402a171d7aeb0c1d2c
Roy Rutto <royrutto@gmail.com>
2015-05-08 17:47:02 +0300

    RR: include leaflet css

--- src/clj/ona/viewer/views/template.clj
+++ src/clj/ona/viewer/views/template.clj
@@ -2,24 +2,26 @@
   (:require [hiccup.page :refer [html5]]
             [ona.utils.tags :as tags]))
 
-(def style-sheets
+(def default-css
   (map #(str "/css/" %)
        ["normalize.css"
         "font-awesome.min.css"
         "pure-min.css"
         "style.css"
         "font-awesome.min.css"
-        "proxima-nova.css"
-        "slick.grid.css"
-        "slick-default-theme.css"]))
+        "proxima-nova.css"]))
 
 (defn base
   [title content & js]
   (html5
   [:head
    [:title title]
-   (for [style-sheet style-sheets]
+   (for [style-sheet default-css]
      [:link {:rel "stylesheet" :type "text/css" :href style-sheet}])]
+  [:link {:rel  "stylesheet"
+          :href "//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css"}]
+  [:link {:rel  "stylesheet" :href "/css/slick.grid.css"}]
+  [:link {:rel  "stylesheet" :href "/css/slick-default-theme.css"}]
   [:body
    [:content {:class "wrapper cfix"}
     [:div {:id "app"}
