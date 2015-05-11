fff8603d6ec8eb140ae33154ccfc256e6d233a04
Roy Rutto <royrutto@gmail.com>
2015-05-11 18:53:50 +0300

    RR: remove comment

--- phantom/unit-test.js
+++ phantom/unit-test.js
@@ -31,7 +31,7 @@ page.open(url, function (status) {
     }
     else {
         console.log("*** Tests failed! ***");
-        // return phantom.exit(1);
+        return phantom.exit(1);
     }
     
     phantom.exit(failures?100:0);
